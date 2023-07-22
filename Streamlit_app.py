8a10488bb950766d30fbd455d6b60d080878414e
AbQadirr <112662842+AbQadirr@users.noreply.github.com>
2023-07-22 00:03:33 -0700

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -45,7 +45,6 @@ webrtc_ctx = webrtc_streamer(
     mode=WebRtcMode.SENDRECV,
     rtc_configuration=RTC_CONFIGURATION,
     media_stream_constraints={"video": True, "audio": False},
-    video_processor_factory=VideoProcessor,
     async_processing=True,
 )
 
