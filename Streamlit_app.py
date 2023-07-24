87ffcb27dffc46298c1ac0b0f273d88008bd37ae
AbQadirr <112662842+AbQadirr@users.noreply.github.com>
2023-07-23 22:44:02 -0700

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -12,32 +12,32 @@ RESIZE_HEIGHT = 300
 
 
 def process_uploaded_image(uploaded_file):
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        # Convert RGBA to RGB if image has an alpha channel
-        if image.mode == 'RGBA':
-            image = image.convert('RGB')
+   if uploaded_file is not None:
+       image = Image.open(uploaded_file)
+       # Convert RGBA to RGB if image has an alpha channel
+       if image.mode == 'RGBA':
+           image = image.convert('RGB')
 
-        image = image.resize((RESIZE_WIDTH, RESIZE_HEIGHT))
-        image_path = "uploaded_image.jpg"  # Save the resized image to a file
-        image.save(image_path)
-        return image_path
-    return None
+       image = image.resize((RESIZE_WIDTH, RESIZE_HEIGHT))
+       image_path = "uploaded_image.jpg"  # Save the resized image to a file
+       image.save(image_path)
+       return image_path
+   return None
 
 
 
 def process_camera_snapshot():
-   video_capture = cv2.VideoCapture(0+cv2.CAP_V4L2)
-    # while(video_capture.isOpened()):
-    ret, frame = video_capture.read()
-    if ret:
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image = Image.fromarray(frame)
-        image = image.resize((RESIZE_WIDTH, RESIZE_HEIGHT))
-        snapshot_path = "snapshot.jpg"  # Save the resized snapshot image to a file
-        image.save(snapshot_path)
-        return snapshot_path
-    return None
+  video_capture = cv2.VideoCapture(0+cv2.CAP_V4L2)
+   # while(video_capture.isOpened()):
+  ret, frame = video_capture.read()
+   if ret:
+       frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+       image = Image.fromarray(frame)
+       image = image.resize((RESIZE_WIDTH, RESIZE_HEIGHT))
+       snapshot_path = "snapshot.jpg"  # Save the resized snapshot image to a file
+       image.save(snapshot_path)
+       return snapshot_path
+   return None
 
 
 def main():
